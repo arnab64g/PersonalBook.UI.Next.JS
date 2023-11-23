78da83d10e5c089d32e4f6203bad2a498c26d17d
Arnab Goswami <79049651+arnab64g@users.noreply.github.com>
2023-11-23 18:27:19 +0600

    ok

--- personal-book/src/app/education/secondary/deleteSecondaryResult.js
+++ personal-book/src/app/education/secondary/deleteSecondaryResult.js
@@ -1,6 +1,26 @@
+import { getToken } from "@/app/tokenHandle/tokenHandle";
 import { Button } from "@mui/material";
 
 export default function DeleteResult({result, isOpen}) {
+    const deleteRes = async () => {
+        const requestOptions = {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' ,
+                        'authorization' : `bearer ${getToken()}` }
+        };
+        const res = await fetch(`http://localhost:7108/api/SecondaryResult?id=${result.id}`, requestOptions);
+        const result1 = await res.json();
+
+        if (result1) {
+            alert("Deleted successed");
+        }
+        else{
+            alert("Unable to delete.")
+        }
+
+        isOpen(false);
+    }
+
     return(<>
     <h1 className="head">Are you sure you want to delete this?</h1>
     <table className="delete-table">
@@ -10,13 +30,13 @@ export default function DeleteResult({result, isOpen}) {
         </tr>
         <tr>
             <td> {result.gradeName} </td>
-            <td>{result.points}</td>
+            <td> {result.points} </td>
         </tr>
     </table>
     <div className="field">
         <Button className="double" variant="outlined" onClick={() => {isOpen(false)}}> Cancel</Button>
         <label className="gap"></label>
-        <Button variant="contained" className="double delete-button"> Delete</Button>
+        <Button variant="contained" className="double delete-button" onClick={deleteRes}> Delete</Button>
     </div>
     </>);
 }
